67b58658723dc05819cc91165088f639c12b962b
Chinmay Srivastava <61424879+srivastavachinmay@users.noreply.github.com>
2021-10-30 22:11:51 +0530

    banner completed

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,10 +2,33 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = () => {
-    return (
-        <div>
+    function truncate(string,n){
+        return string?.length>n?string.substr(0,n-1)+'...':string;
+    }
 
-        </div>
+    return (
+        <header className={'banner'} style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+            backgroundPosition: 'center center'
+        }}>
+            <div className={'banner__contents'}>
+                <h1 className={'banner__title'}>Movie Name</h1>
+                <div className={'banner__buttons'}>
+                    <button className={'banner__button'}>Play</button>
+                    <button className={'banner__button'}>My List</button>
+                </div>
+                <h1 className={'banner__description'}>{truncate(`This is a test descriptionThis is a test description
+                    This is a test description
+                    This is a test description
+                    This is a test description
+                    This is a test descriptionThis is a test description
+                    This is a test description
+                    This is a test description`,150)}
+                </h1>
+            </div>
+            <div className={'banner__fadeBottom'}/>
+        </header>
     );
 };
 
